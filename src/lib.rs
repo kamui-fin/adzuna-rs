95bc39783af8d015474140213d2dc56a3856333d
Kamui <fin-kamui@pm.me>
2023-02-03 14:28:40 -0600

    feat: add all parameters and models

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,9 @@
 use reqwest::StatusCode;
 use serde::de::DeserializeOwned;
 use serde::{Deserialize, Serialize};
+use std::fmt::Display;
+use std::fmt::Formatter;
 use std::marker::PhantomData;
-
-/* get jobs/{country}/search/{page}
-Search the Adzuna job database
-
-get jobs/{country}/categories
-List available categories
-
-get jobs/{country}/histogram
-Provide histogram data of salary data
-
-get jobs/{country}/top_companies
-List the top employers for the search terms supplied
-
-get jobs/{country}/geodata
-Provides salary data for locations inside an area
-
-get jobs/{country}/history
-Provides historical average salary data
-
-get version
-Returns the current version of this API */
 
 const ROOT_URL: &str = "https://api.adzuna.com/v1/api";
 
@@ -90,13 +71,13 @@ impl Country {
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub struct VersionResponse {
+pub struct ApiVersion {
     pub api_version: u8,
     pub software_version: String,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub struct LeaderboardItem {
+pub struct Company {
     count: usize,
     canonical_name: String,
     average_salary: usize,
@@ -104,8 +85,8 @@ pub struct LeaderboardItem {
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub struct TopCompanyResponse {
-    pub leaderboard: Vec<LeaderboardItem>,
+pub struct TopCompaniesResponse {
+    pub leaderboard: Vec<Company>,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
@@ -119,9 +100,78 @@ pub struct CategoriesResponse {
     results: Vec<Category>,
 }
 
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct HistoryResponse {
+    month: f64,
+    what: String,
+    location: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct HistogramResponse {
+    location: String,
+    histogram: f64,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct LocationDetail {
+    area: Vec<String>,
+    display_name: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Location {
+    count: usize,
+    location: LocationDetail,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct GeodataResponse {
+    locations: Vec<Location>,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Job {
+    id: String,
+    created: String,
+    title: String,
+    description: String,
+    full_description: String,
+    redirect_url: String,
+    latitude: f64,
+    longitude: f64,
+    category: Category,
+    location: LocationDetail,
+    salary_min: u64,
+    salary_max: u64,
+    salary_is_predicted: u64, // ?
+    company: Company,
+    contract_type: String,
+    contract_time: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct SearchResponse {
+    results: Vec<Job>,
+    mean: f64,
+}
+
 pub enum SortDirection {
     Up,
     Down,
+}
+
+impl Display for SortDirection {
+    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+        match self {
+            SortDirection::Up => {
+                write!(f, "up")
+            }
+            SortDirection::Down => {
+                write!(f, "down")
+            }
+        }
+    }
 }
 
 pub enum SortBy {
@@ -132,6 +182,29 @@ pub enum SortBy {
     Relevance,
 }
 
+impl Display for SortBy {
+    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+        match self {
+            SortBy::Default => {
+                write!(f, "default")
+            }
+            SortBy::Hybrid => {
+                write!(f, "hybrid")
+            }
+            SortBy::Date => {
+                write!(f, "date")
+            }
+            SortBy::Salary => {
+                write!(f, "salary")
+            }
+            SortBy::Relevance => {
+                write!(f, "relevance")
+            }
+        }
+    }
+}
+
+#[derive(Default, Serialize)]
 pub struct Parameters {
     what: Option<String>,
     months: Option<usize>,
@@ -143,21 +216,29 @@ pub struct Parameters {
     what_exclude: Option<String>,
     title_only: Option<String>,
     r#where: Option<String>,
-    distance: Option<usize>,
-    results_per_page: Option<usize>,
-    max_days_old: Option<usize>,
-    sort_dir: Option<SortDirection>,
-    sort_by: Option<SortBy>,
-    salary_min: Option<usize>,
-    salary_max: Option<usize>,
     salary_include_unknown: Option<String>,
     full_time: Option<String>,
     part_time: Option<String>,
     contract: Option<String>,
     permanent: Option<String>,
     company: Option<String>,
+    distance: Option<usize>,
+    results_per_page: Option<usize>,
+    max_days_old: Option<usize>,
+    salary_min: Option<usize>,
+    salary_max: Option<usize>,
+    sort_dir: Option<String>,
+    sort_by: Option<String>,
 
-    locations: Option<Vec<String>>, // TODO: flatten out
+    location0: Option<String>,
+    location1: Option<String>,
+    location2: Option<String>,
+    location3: Option<String>,
+    location4: Option<String>,
+    location5: Option<String>,
+    location6: Option<String>,
+    location7: Option<String>,
+
     category: Option<String>,
 }
 
@@ -165,6 +246,7 @@ pub struct RequestBuilder<'a, T> {
     client: &'a Client,
     endpoint: &'static str,
     search_country: Option<&'static str>,
+    parameters: Parameters,
     __phantom: PhantomData<T>,
 }
 
@@ -174,12 +256,146 @@ impl<'a, T> RequestBuilder<'a, T> {
             client,
             endpoint,
             search_country: None,
+            parameters: Default::default(),
             __phantom: PhantomData,
         }
     }
 
+    pub fn what(mut self, what: &str) -> Self {
+        self.parameters.what = Some(what.into());
+        self
+    }
+
+    pub fn what_and(mut self, what_and: &str) -> Self {
+        self.parameters.what_and = Some(what_and.into());
+        self
+    }
+
+    pub fn what_phrase(mut self, what_phrase: &str) -> Self {
+        self.parameters.what_phrase = Some(what_phrase.into());
+        self
+    }
+
+    pub fn what_or(mut self, what_or: &str) -> Self {
+        self.parameters.what_or = Some(what_or.into());
+        self
+    }
+
+    pub fn what_exclude(mut self, what_exclude: &str) -> Self {
+        self.parameters.what_exclude = Some(what_exclude.into());
+        self
+    }
+
+    pub fn r#where(mut self, r#where: &str) -> Self {
+        self.parameters.r#where = Some(r#where.into());
+        self
+    }
+
+    pub fn title_only(mut self, title_only: &str) -> Self {
+        self.parameters.title_only = Some(title_only.into());
+        self
+    }
+
+    pub fn full_time(mut self, full_time: &str) -> Self {
+        self.parameters.full_time = Some(full_time.into());
+        self
+    }
+
+    pub fn part_time(mut self, part_time: &str) -> Self {
+        self.parameters.part_time = Some(part_time.into());
+        self
+    }
+
+    pub fn contract(mut self, contract: &str) -> Self {
+        self.parameters.contract = Some(contract.into());
+        self
+    }
+
+    pub fn company(mut self, company: &str) -> Self {
+        self.parameters.company = Some(company.into());
+        self
+    }
+
+    pub fn distance(mut self, distance: usize) -> Self {
+        self.parameters.distance = Some(distance);
+        self
+    }
+
+    pub fn results_per_page(mut self, results_per_page: usize) -> Self {
+        self.parameters.results_per_page = Some(results_per_page);
+        self
+    }
+
+    pub fn max_days_old(mut self, max_days_old: usize) -> Self {
+        self.parameters.max_days_old = Some(max_days_old);
+        self
+    }
+
+    pub fn salary_min(mut self, distance: usize) -> Self {
+        self.parameters.salary_min = Some(distance);
+        self
+    }
+
+    pub fn salary_max(mut self, salary_max: usize) -> Self {
+        self.parameters.salary_max = Some(salary_max);
+        self
+    }
+
+    pub fn permanent(mut self, permanent: &str) -> Self {
+        self.parameters.permanent = Some(permanent.into());
+        self
+    }
+
+    pub fn sort_by(mut self, sort_by: SortBy) -> Self {
+        self.parameters.sort_by = Some(sort_by.to_string());
+        self
+    }
+
+    pub fn sort_dir(mut self, sort_dir: SortDirection) -> Self {
+        self.parameters.sort_dir = Some(sort_dir.to_string());
+        self
+    }
+
+    pub fn salary_include_unknown(mut self, salary_include_unknown: &str) -> Self {
+        self.parameters.salary_include_unknown = Some(salary_include_unknown.into());
+        self
+    }
+
+    pub fn months(mut self, months: usize) -> Self {
+        self.parameters.months = Some(months);
+        self
+    }
+
+    pub fn category(mut self, category: &str) -> Self {
+        self.parameters.category = Some(category.into());
+        self
+    }
+
     pub fn country(mut self, country: Country) -> Self {
         self.search_country = Some(country.to_code());
+        self
+    }
+
+    pub fn location(mut self, location: &str) -> Self {
+        if self.parameters.location0.is_none() {
+            self.parameters.location0 = Some(location.to_string());
+        } else if self.parameters.location1.is_none() {
+            self.parameters.location1 = Some(location.to_string());
+        } else if self.parameters.location2.is_none() {
+            self.parameters.location2 = Some(location.to_string());
+        } else if self.parameters.location3.is_none() {
+            self.parameters.location3 = Some(location.to_string());
+        } else if self.parameters.location4.is_none() {
+            self.parameters.location4 = Some(location.to_string());
+        } else if self.parameters.location5.is_none() {
+            self.parameters.location5 = Some(location.to_string());
+        } else if self.parameters.location6.is_none() {
+            self.parameters.location6 = Some(location.to_string());
+        } else if self.parameters.location7.is_none() {
+            self.parameters.location7 = Some(location.to_string());
+        } else {
+            // location already full
+        }
         self
     }
 
@@ -198,7 +414,12 @@ impl<'a, T> RequestBuilder<'a, T> {
         ];
 
         let client = reqwest::Client::new();
-        let response = client.get(url).query(&auth_params).send().await;
+        let response = client
+            .get(url)
+            .query(&auth_params)
+            .query(&self.parameters)
+            .send()
+            .await;
 
         match &response {
             Ok(r) => {
@@ -229,6 +450,12 @@ impl<'a, T> RequestBuilder<'a, T> {
     }
 }
 
+// TODO:
+// - verify and test all models
+// - rename (take out *Response part)
+// - add docs for each model, route, and parameter
+// - apply post processing to results
+
 impl Client {
     pub fn new(app_id: String, app_key: String) -> Self {
         Self { app_id, app_key }
@@ -238,12 +465,32 @@ impl Client {
         RequestBuilder::<T>::new(self, endpoint)
     }
 
-    pub fn api_version(&self) -> RequestBuilder<VersionResponse> {
+    pub fn api_version(&self) -> RequestBuilder<ApiVersion> {
         self.query("version")
     }
 
     pub fn categories(&self) -> RequestBuilder<CategoriesResponse> {
         self.query("categories").country(Country::UnitedStates) // default country
+    }
+
+    pub fn history(&self) -> RequestBuilder<HistoryResponse> {
+        self.query("history").country(Country::UnitedStates)
+    }
+
+    pub fn geodata(&self) -> RequestBuilder<GeodataResponse> {
+        self.query("geodata").country(Country::UnitedStates)
+    }
+
+    pub fn top_companies(&self) -> RequestBuilder<TopCompaniesResponse> {
+        self.query("top_companies").country(Country::UnitedStates)
+    }
+
+    pub fn histogram(&self) -> RequestBuilder<HistogramResponse> {
+        self.query("histogram").country(Country::UnitedStates)
+    }
+
+    pub fn search(&self) -> RequestBuilder<SearchResponse> {
+        self.query("search").country(Country::UnitedStates)
     }
 }
 
